55c821509904246b1abfc163aaa043e5143ed3ef
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-12-01 23:50:48 -0200

    sem spawn

--- Assets/scripts/SpawnInimigo.cs
+++ Assets/scripts/SpawnInimigo.cs
@@ -12,14 +12,30 @@ public class SpawnInimigo : MonoBehaviour
     public GameObject iscaPrefab;
     public GameObject redeDePescaPrefab;
 
-
-    private float intervaloSpawn=0;
-
-
-    private float intervaloSpawnInicial;
+    private float spawnarIscaInicial;
 
     [SerializeField]
-    private float intervaloSpawnMax;
+    private float spawnarIscaMax;
+
+    private float spawnarTubaraoInicial;
+
+    [SerializeField]
+    private float spawnarTubaraoMax;
+
+    private float spawnarRedePescaInicial;
+
+    [SerializeField]
+    private float spawnaRedePescaMax;
+
+
+
+    //private float intervaloSpawn=0;
+
+
+    //private float intervaloSpawnInicial;
+
+    //[SerializeField]
+    //private float intervaloSpawnMax;
 
 
 
@@ -30,10 +46,10 @@ public class SpawnInimigo : MonoBehaviour
     [SerializeField]
     float distanceEnemyFromPlayer;
 
-    private float timerRespawnInimigos;
+    //private float timerRespawnInimigos;
 
-    [SerializeField]
-    private float timerRespawnInimigosMax;
+    //[SerializeField]
+    //private float timerRespawnInimigosMax;
     [SerializeField]
     private Transform player_ref; 
 
@@ -57,34 +73,52 @@ public class SpawnInimigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time >= intervaloSpawnInicial + intervaloSpawnMax && intervaloSpawn == 0)
-        {
-            intervaloSpawn = 1;  
-        }
-        if (Time.time >= timerRespawnInimigos + timerRespawnInimigosMax && intervaloSpawn==1)
-        {
+          /*if (Time.time >= intervaloSpawnInicial + intervaloSpawnMax && intervaloSpawn == 0)
+          {
+             intervaloSpawn = 1;  
+          }
+          if (Time.time >= timerRespawnInimigos + timerRespawnInimigosMax && intervaloSpawn==1)
+          */
+
             
 
-            timerRespawnInimigos = Time.time;
-
-            intervaloSpawn = 0;
+        if (Time.time >= spawnarIscaInicial + spawnarIscaMax)
+        {
+            spawnarIscaInicial = Time.time;
 
             Vector2 initialPos = player_ref.transform.position;
             Vector2 position = initialPos;
             position.x += distanceEnemyFromPlayer;
-            position.y = groundLevel;// aqui o y é sempre o mesmo
+            position.y = groundLevel;
 
+            SpawnarInimigos<Isca>(1, 2, 5, Random.Range(-24, 14.137f), position);
+        }
+        if (Time.time >= spawnarTubaraoInicial + spawnarTubaraoMax)
+        {
+            spawnarTubaraoInicial = Time.time;
 
+            Vector2 initialPos = player_ref.transform.position;
+            Vector2 position = initialPos;
+            position.x += distanceEnemyFromPlayer;
+            position.y = groundLevel;
 
-            SpawnarInimigos<Isca>(1, 1, 6,Random.Range(-26, 19.137f), position);// o 1,6 são respectivamente a distanceMin e distanceMax entre os inimigos?
-            SpawnarInimigos<Tubarao>(1, 1, 6, Random.Range(-26, 19.137f), position);
-            SpawnarInimigos<RedePesca>(1, 1, 6, Random.Range(-26, 19.137f), position);
+            SpawnarInimigos<Tubarao>(1, 3, 4, Random.Range(-22, 15.137f), position);
+        }
+        if (Time.time >= spawnarRedePescaInicial + spawnaRedePescaMax)
+        {
+            spawnarRedePescaInicial = Time.time;
+
+            Vector2 initialPos = player_ref.transform.position;
+            Vector2 position = initialPos;
+            position.x += distanceEnemyFromPlayer;
+            position.y = groundLevel;
+
+            SpawnarInimigos<RedePesca>(1, 1, 6, Random.Range(-12, 14.137f), position);
         }
     }
     public void SpawnarInimigos<Y>(int quantidadeIinimigos, float distanceMin, float distanceMax, float heightMax, Vector2 initialPos)
     {
-        //if (!typeof(Y).IsSubclassOf(typeof(GameObject)))
-            //return; 
+       
 
             
             
@@ -102,41 +136,42 @@ public class SpawnInimigo : MonoBehaviour
                        {
                            int possicao = ListInimigos.FindLastIndex(x => x.GetType() == typeof(Isca));
 
-                           Isca I = (Isca)ListInimigos[possicao];
+                           GameObject Isca  = ListInimigos[possicao];
                            ListInimigos.RemoveAt(possicao);
 
-                           I.transform.position = position;
+                           Isca.transform.position = position;
+                           Isca.SetActive(true);
                            
 
                        }
                     }
-                    /*else if (typeof(Y) == typeof(RedePesca))
+                    else if (typeof(Y) == typeof(RedePesca))
                     {
                         if (ListInimigos.OfType<RedePesca>().Any())
                         {
                            int possicao = ListInimigos.FindLastIndex(x => x.GetType() == typeof(RedePesca));
 
-                           RedePesca R = (RedePesca)ListInimigos[possicao];
+                           GameObject RedePesca = ListInimigos[possicao];
 
                            ListInimigos.RemoveAt(possicao);
 
-                           R.transform.position = position;
-                           R.SetActive(true);
+                           RedePesca.transform.position = position;
+                           RedePesca.SetActive(true);
                           
                              
                         }
-                    }*/
+                    }
                     else if (typeof(Y) == typeof(Tubarao))
                     {
                          if (ListInimigos.OfType<Tubarao>().Any())
                          {
                               int possicao = ListInimigos.FindLastIndex(x => x.GetType() == typeof(Tubarao));
 
-                              Tubarao T = (Tubarao)ListInimigos[possicao];
+                              GameObject tubarao = ListInimigos[possicao];
                               ListInimigos.RemoveAt(possicao);
 
-                              T.transform.position = position;
-
+                              tubarao.transform.position = position;
+                              tubarao.SetActive(true);
                                 
                          } 
                     }
@@ -145,16 +180,16 @@ public class SpawnInimigo : MonoBehaviour
                 {
                     if (typeof(Y) == typeof(Isca))
                     {
-                        HabilidadesGeraisInimigo isca = Instantiate(iscaPrefab, position, Quaternion.identity).GetComponent<Isca>();
+                        Isca GameObject  = Instantiate(iscaPrefab, position, Quaternion.identity).GetComponent<Isca>();
                     }
                     if (typeof(Y) == typeof(Tubarao))
                     {
-                        HabilidadesGeraisInimigo Tubarao = Instantiate(tubaraoPrefab, position, Quaternion.identity).GetComponent<Tubarao>();    
+                        Tubarao GameObject = Instantiate(tubaraoPrefab, position, Quaternion.identity).GetComponent<Tubarao>();    
                     }
-                    /*if (typeof(Y) == typeof(RedePesca))
+                    if (typeof(Y) == typeof(RedePesca))
                     {
-                        HabilidadesGeraisInimigo RedePesca = Instantiate(redeDePescaPrefab, position, Quaternion.identity).GetComponent<RedePesca>();
-                    }*/
+                        RedePesca GameObject = Instantiate(redeDePescaPrefab, position, Quaternion.identity).GetComponent<RedePesca>();
+                    }
                 }
                 
 
@@ -162,18 +197,19 @@ public class SpawnInimigo : MonoBehaviour
             
         
     }
-    public void adicionarOuDestruir(GameObject inimigo)                                         
+    public void adicionarOuDestruir(GameObject gameObject)                                         
     {
              
         if (ListInimigos.Count > 0)
         {
-            ListInimigos.Add(inimigo);
+            ListInimigos.Add(gameObject);
         }
         else
         {
-            Destroy(inimigo);
+            Destroy(gameObject);
         }
     }
+    
    
 
 
